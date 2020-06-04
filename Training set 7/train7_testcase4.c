950a6fb01895994c651c84ba2b12d7b0d903403d
unknown <akhourinupur12345@gmail.com>
2020-06-04 13:10:32 +0530

    Training set 7

---
+++ Training set 7/train7_testcase4.c
@@ -0,0 +1,190 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <ctype.h>
+char town[] = "VILL: RUPA KI NANGAL";
+struct Huff {
+int freq;
+char alpha;
+struct Huff *left;
+struct Huff *right;
+struct Huff *parent;
+};
+char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+/*int freq[] = {21912, 16587, 14810, 14003, 13318, 12666,
+11450, 10977, 10795, 7874, 7253, 5246, 4943,
+4761, 4200, 3853, 3819, 3693, 3316, 2715,
+2019, 1257, 315, 205, 188, 128 };*/
+/* Other test cases
+int freq[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,
+14,15,16,17,18,19,20,21,22,23,24,25,26};
+int freq[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+1,1,1,1,1,1,1};*/
+int freq[] = {3,0,0,0,0,0,1,0,2,0,2,3,0,2,0,1,0,
+1,0,0,1,1,0,0,0,0};
+
+struct Huff alphabet[26]; // Fixed array to store letters
+// References to nodes awaiting to be in Huffman tree
+// Roots of trees in a forest
+struct Huff *heap[30];
+// and count of entries in heap
+int heapSz = 0; // index 0 is not used in this heap
+// To re-establish heap property after an insertion
+void heapifyUp(int index);
+// To re-establish heap property after a deletion
+void heapifyDown(int index);
+void initAlphaNodes(void) {
+/* Fill freq data in array alphabet
+Alphabets are nodes outside our Huffman tree
+Leaf nodes in the heap will have
+reference to these entities in their
+*/
+int i;
+for (i=0; i<26; i++) {
+alphabet[i].alpha = letters[i];
+alphabet[i].freq = freq[i];
+alphabet[i].left = NULL;
+alphabet[i].right = NULL;
+}
+}
+struct Huff *makeHuffNode(int freq, struct Huff *left,
+struct Huff *right) {
+struct Huff *newNode = malloc(sizeof(struct Huff));
+assert(newNode != NULL);
+// Will use this to keep coding information
+newNode->alpha = ' ';
+newNode->freq = freq;
+newNode->right = right;
+newNode->left = left;
+if (left != NULL) {
+left->parent = newNode;
+// If not pointing to an entry in array alphabet
+if (left->alpha == ' ')
+left->alpha = '0';
+}
+if (right != NULL) {
+right->parent = newNode;
+right->alpha = '1';
+}
+return newNode;
+}
+void addToHeap(struct Huff *newNode) {
+assert(heapSz < 30);
+heap[++heapSz] = newNode;
+heapifyUp(heapSz); // Restore heap property
+}
+struct Huff *removeMin(void) {
+assert(heapSz>1);
+struct Huff *tmp = heap[1];
+heap[1] = heap[heapSz--];
+heapifyDown(1); // Restore heap property
+return tmp;
+}
+void initHeapLeaves(void) {
+int i;
+for (i=0; i<26; i++) {
+addToHeap(makeHuffNode(alphabet[i].freq, &alphabet[i], NULL));
+}
+}
+void swapAtIdx(int x, int y) {
+/* Used by heapify functions */
+struct Huff *tmp;
+tmp = heap[x];
+heap[x] = heap[y];
+heap[y] = tmp;
+}
+void heapifyUp(int index)
+{
+/* Removed about 10 lines of code
+See Figure 27.7 in Reema Thareja textbook */
+if(index<=1)
+    return;
+if(heap[index]->freq>heap[index/2]->freq)
+    return;
+swapAtIdx(index/2,index);
+heapifyUp(index/2);
+return;
+}
+void heapifyDown(int index) {
+/* Removed 10 to 15 lines of code
+See Figure 12.10 in Reema Thareja */
+int left_child=2*index;
+int right_child=left_child+1;
+if(left_child>heapSz)
+    return;
+if(right_child>heapSz)
+{
+    if(heap[index]->freq>heap[left_child]->freq)
+        swapAtIdx(index,left_child);
+    return;
+}
+if((heap[index]->freq>heap[left_child]->freq) && (heap[index]->freq>heap[right_child]->freq))
+{
+    if(heap[left_child]->freq < heap[right_child]->freq)
+    {
+        swapAtIdx(index,left_child);
+        heapifyDown(left_child);
+    }
+    else
+    {
+        swapAtIdx(index,right_child);
+        heapifyDown(right_child);
+    }
+
+}
+else if(heap[left_child]->freq<heap[index]->freq)
+{
+    swapAtIdx(index,left_child);
+    heapifyDown(left_child);
+}
+else if(heap[right_child]->freq<heap[index]->freq)
+{
+    swapAtIdx(index,right_child);
+    heapifyDown(right_child);
+}
+return;
+}
+int heapIsRootOnly(void) { // Huffman tree is ready?
+return heapSz == 1;
+}
+void printLetterCode(char c) {
+int i = c-'A';
+struct Huff *bit = alphabet[i].parent;
+while (bit != NULL) {
+printf("%c", bit->alpha);
+bit = bit->parent;
+}
+}
+int main(void) {
+struct Huff *left, *right, *newNode, *node;
+int i;
+initAlphaNodes();
+initHeapLeaves();
+while (!heapIsRootOnly()) {
+left = removeMin();
+if (heapIsRootOnly()) {
+right = heap[1];
+heap[1] =
+makeHuffNode(left->freq+right->freq, left, right);
+break;
+}
+right = removeMin();
+addToHeap(makeHuffNode(left->freq+right->freq,
+left, right));
+}
+/* Code town */
+printf("Words in %s are coded as follows\n", town);
+i =-1;
+while (town[++i] != '\0')
+if (isspace(town[i]))
+printf("\n");
+else if (!isupper(town[i]))
+printf("\n%c", town[i]);
+else
+printLetterCode(town[i]);
+}
+
+
+
---
+++ Training set 7/train8task3.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <limits.h>
+#include <ctype.h>
+#include <math.h>
+#define TBLSZ 5000
+// Hashing choice related constants
+#define DIVISION '%'
+#define MULTIPLICATIVE '*'
+#define LINEAR 1
+// Step size for linear probing. Use only primes
+#define STEP 2
+#define DOUBLE_HASHING 2
+#define QUADRATIC_REHASH 4
+// To be set by the students as needed
+#define PRIMARY_HASHING_METHOD MULTIPLICATIVE
+#define PROBING_METHOD QUADRATIC_REHASH
+// The following are primes < TBLSZ
+int usedSize, // Part of the table that will be used
+probeStepSz; // Step size for double hashing
+double goldenNumber;
+char *table[TBLSZ];
+int initialise(int tableSize)
+{
+/* Find two suitable prime values for
+two division hashings funtions.
+*/
+/* usedSize should be a prime that is nearly
+as big as TBLSZ
+This sets the part of the table that is actually
+used to store pointers to strings
+*/
+    assert(TBLSZ > 1000);
+    assert(PROBING_METHOD >=0);
+    assert(PROBING_METHOD < 5);
+    assert(PRIMARY_HASHING_METHOD == MULTIPLICATIVE ||
+    PRIMARY_HASHING_METHOD == DIVISION);
+    goldenNumber = (sqrt(5.0)+1)/2;
+    int testing, prime, i;
+    if (TBLSZ%2 == 0)
+        usedSize = TBLSZ-1;
+    else
+        usedSize = TBLSZ;
+    /* Begin with usedSize as big as TBLSZ and
+    progressively lower it till it is a prime
+    Pointers to words/strings will be stored
+    in index range 0 to usedSize-1 only.
+    */
+    while (usedSize > 2)
+    { // Keep trying
+        i = 3;
+        while (i*i <= usedSize)
+        {
+            if (usedSize%i == 0)
+            {
+                /* No good */
+                usedSize -= 2;
+                break;
+            }
+            i += 2;
+        }
+        if (i*i > usedSize)
+        {
+            assert(usedSize > 1000);
+            assert(usedSize < TBLSZ);
+            break;
+        }
+    }
+    /* The following code finds a prime to use to do
+    probes over the table when there is a collision */
+    probeStepSz = 2;
+    while (probeStepSz*probeStepSz < usedSize)
+        probeStepSz *= 2;
+    /* Similar to the previous search for a prime */
+    while (probeStepSz > 2)
+    { // Keep trying
+        i = 3;
+        while (i*i <= probeStepSz)
+        {
+            if (probeStepSz%i == 0)
+            {
+                /* No good */
+                probeStepSz -= 2;
+                break;
+            }
+            i += 2;
+        }
+        if (i*i > probeStepSz)
+        {
+            assert(probeStepSz > 1);
+            assert(probeStepSz < usedSize);
+            break;
+        }
+    }
+}
+int goldenMultiplicative(int key, int size)
+{
+/* Returns the primary location for the data
+in the hash table.
+The location is computed using
+multiplication by golden number.
+Reference: Reema Thareja Edn 2 page 468
+*/
+    double multiplier = goldenNumber - 1;
+    double frac = (key*multiplier);
+    int index = frac;
+    frac = frac-index; // Get fractional part
+    index = frac*size;
+    assert(index >= 0);
+    assert(index < size);
+    return index;
+}
+int makeNumber(char *str)
+{
+/* ********************************************
+Students to use code - implementation not important
+Construct a number from the string
+The function may return the same number for
+several different strings
+However, it will alway return the same number
+for a string
+**********************************************/
+    assert(str != NULL);
+    int num = 0;
+    int el = strlen(str);
+    int i = el - 1; // Next letter to process
+    if (el%2 == 0) el--; // Make it odd
+    assert(el < 20 && el > 0); // Too long a word
+    /* Computer specific limit on while */
+    while(num < INT_MAX/26/26)
+    {
+        num = num*26+toupper(str[i])-toupper('A');
+        i = (i+2)%el; // Choose alternate letters
+    }
+    num = 26*num+el; // Include word length
+    return num;
+}
+int indexAdvisedByHash(int key, int probe)
+{
+    /* What arrangement has been chosen */
+    int step, goldIndx;
+    switch (PRIMARY_HASHING_METHOD)
+    {
+        case DIVISION:
+            switch (PROBING_METHOD)
+            {
+                case LINEAR:
+                    return (key + STEP*probe)%usedSize;
+                case DOUBLE_HASHING:
+                    step = key%probeStepSz;
+                // step != 0 is necessary
+                    if (step < 2)
+                        step = 17;
+                    return (key + probe*step)%usedSize;
+                case QUADRATIC_REHASH:
+                    return (key + STEP*probe + probeStepSz*probe*probe)%usedSize;
+                default:
+                    assert(0); // Unexpected
+                return 0;
+            }
+        case MULTIPLICATIVE:
+        goldIndx = goldenMultiplicative(key, usedSize);
+        switch (PROBING_METHOD)
+        {
+            case QUADRATIC_REHASH:
+                    return (key + STEP*probe + probeStepSz*probe*probe)%usedSize;
+            case LINEAR:
+                return (goldIndx + STEP*probe)%usedSize;
+            case DOUBLE_HASHING:
+                step = goldenMultiplicative(key,
+                probeStepSz);
+                // step != 0 is necessary
+                if (step < 2)
+                    step = 1009;
+                return (goldIndx + probe*step)%usedSize;
+            default:
+                assert(0); // Not expected
+            return 0;
+        }
+        default:
+            assert(0); // ERROR
+    }
+}
+int main(void)
+{
+    FILE *collection = fopen("collection.txt", "r");
+    assert(collection != NULL);
+    char strg[100];
+    char * dataP;
+    int loc, collisions=0, noCollisions=0;
+    int el, reTry, rehash, filled;
+    initialise(TBLSZ);
+    /* Read word from FILE collection and
+    store pointer to word in array table[].
+    Array index to be determined by a
+    hashing method.
+    */
+    while (fgets(strg,90,collection))
+    {
+        el= strlen(strg)-1;
+        strg[el] = '\0';
+        dataP = malloc(strlen(strg)+1);
+        assert(dataP != NULL);
+        strcpy(dataP, strg);
+        /* reTry == 0 is primary hashing attempt.
+        reTry > 0 are attempted alternate locations.
+        */
+        reTry = 0;
+        while (rehash < 100)
+        {
+            loc = indexAdvisedByHash(makeNumber(strg), reTry);
+            if (table[loc] != NULL)
+            {
+                reTry++;
+                collisions++;
+                continue;
+            }
+            else
+            {
+                filled++;
+                table[loc] = dataP;
+                break;
+            }
+        }
+    }
+    printf("Total collisions %d\n", collisions);
+    printf("Unused %d\n", TBLSZ-filled);
+    printf("Filled %d\n", filled);
+    return 0;
+}
+
+
+
+
